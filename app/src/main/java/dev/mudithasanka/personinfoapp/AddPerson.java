6906904adeff6c9d426daeca93294755701b9f0d
MudithAsanka <mudithasanka7@gmail.com>
2021-05-15 00:53:38 +0530

    Add Person Activity

---
+++ app/src/main/java/dev/mudithasanka/personinfoapp/AddPerson.java
@@ -0,0 +1,14 @@
+package dev.mudithasanka.personinfoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPerson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_person);
+    }
+}
