fa9938020f649b961568e9c4d1f31aa3300b17b5
MudithAsanka <mudithasanka7@gmail.com>
2021-05-15 13:42:27 +0530

    Add Person Activity

--- app/src/main/java/dev/mudithasanka/personinfoapp/AddPerson.java
+++ app/src/main/java/dev/mudithasanka/personinfoapp/AddPerson.java
@@ -3,6 +3,8 @@ package dev.mudithasanka.personinfoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddPerson extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class AddPerson extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_person);
+
+        Spinner addPersonSpinner = (Spinner) findViewById(R.id.spinnerAddPerson);
+
+        ArrayAdapter<String> addPersonAdapter = new ArrayAdapter<String>(AddPerson.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.divisions));
+        addPersonAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        addPersonSpinner.setAdapter(addPersonAdapter);
     }
 }
